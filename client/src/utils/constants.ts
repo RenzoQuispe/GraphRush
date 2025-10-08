e7bc51031712edda13595adbd95e7b4b92c48d33
RenzoQuispe <renzo123cd@gmail.com>
2025-10-08 16:06:18 -0500

    actualizar dependencias y agregar primeras constantes globales

---
+++ client/src/utils/constants.ts
@@ -0,0 +1,25 @@
+// constantes globales del proyecto
+
+export const COLORS = {
+  PALETTE: [
+    '#e21515ff', // Rojo
+    '#2b37c2ff', // Azul
+    '#fbcf20ff', // Amarillo
+    '#2cee13ff', // verde claro
+    '#6c2a88ff', //morado
+    '#f56bebff', // rosado
+    '#bbbbbbff', // gris
+  ],
+  GRAPH: {
+    NODE_DEFAULT: '#1e293b',
+    NODE_STROKE: '#8b5cf6',
+    NODE_STROKE_HOVER: '#fbbf24',
+    EDGE_DEFAULT: '#475569',
+    EDGE_HIGHLIGHT: '#a78bfa',
+  }
+};
+
+export const STORAGE_KEYS = {
+  HIGH_SCORE: 'coloreo-highscore',
+  SOUND_ENABLED: 'sound-enabled',
+};
